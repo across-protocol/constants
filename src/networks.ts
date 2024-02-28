0fb7801df9ba350b2bb09a7c2a9dae75b95d046a
Paul <108695806+pxrl@users.noreply.github.com>
2024-02-28 04:06:31 +0100

    feat: Import public networks definitions from UMA
    Currently the Across FE needs to import PublicNetworks via
    @uma/common/dist due to an issue upstream. This change will instead
    provide chain definitions locally, where they be more easily managed.
    
    As part of this work, the chain definitions have been updated to include
    the latest Sepolia networks that are used by Across.
    
    See also:
    https://github.com/across-protocol/sdk-v2/commit/ca356ce0816922af33f14b99d5ca91e022ca0d59

--- src/networks.ts
+++ src/networks.ts
@@ -20,3 +20,37 @@ export const CHAIN_IDs = {
   BASE_SEPOLIA: 84532,
   ARBITRUM_SEPOLIA: 421614,
 };
+
+interface PublicNetwork {
+  name: string;
+  nativeToken: string;
+  blockExplorer: string;
+}
+
+export const PUBLIC_NETWORKS: { [chainId: number]: PublicNetwork } = {
+  1: { name: "Mainnet",  nativeToken: "ETH", blockExplorer: "https://etherscan.io" },
+  3: { name: "Ropsten", nativeToken: "ETH", blockExplorer: "https://ropsten.etherscan.io" },
+  4: { name: "Rinkeby", nativeToken: "ETH",  blockExplorer: "https://rinkeby.etherscan.io" },
+  5: { name: "Goerli", nativeToken: "ETH", blockExplorer: "https://goerli.ethercan.io" },
+  10: { name: "Optimism", nativeToken: "ETH", blockExplorer: "https://optimistic.ethercan.io" },
+  42: { name: "Kovan", nativeToken: "ETH", blockExplorer: "https://kovan.etherscan.io" },
+  69: { name: "Optimism Kovan", nativeToken: "ETH", blockExplorer: "https://kovan-optimistic.ethercan.io" },
+  100: { name: "Gnosis", nativeToken: "XDAI", blockExplorer: "https://blockscout.comxdai/mainnet" },
+  137: { name: "Polygon",  nativeToken: "MATIC", blockExplorer: "https://polygonscan.com" },
+  280: { name: "zkSync Goerli", nativeToken: "ETH", blockExplorer: "https://goerli.explorer.zksync.io" },
+  288: { name: "Boba", nativeToken: "ETH", blockExplorer: "https://blockexplorer.boba.network" },
+  300: { name: "zkSync Sepolia", nativeToken: "ETH", blockExplorer: "https://sepolia.explorer.zksync.io" },
+  324: { name: "zksync", nativeToken: "ETH", blockExplorer: "https://explorer.zksync.io" },
+  80001: { name: "Polygon Mumbai", nativeToken: "MATIC", blockExplorer: "https://mumbai.polygonscan.com" },
+  42161: { name: "Arbitrum One", nativeToken: "ETH", blockExplorer: "https://arbiscan.io" },
+  43114: { name: "Avalanche", nativeToken: "AVAX", blockExplorer: "https://snowtrace.io" },
+  8453: { name: "Base", nativeToken: "ETH", blockExplorer: "https://basescan.org" },
+  84531: { name: "Base Goerli", nativeToken: "ETH", blockExplorer: "https://goerli.basescan.org" },
+  84532: { name: "Base Sepolia", nativeToken: "ETH", blockExplorer: "https://sepolia.basescan.org" },
+  421611: { name: "Arbitrum Rinkeby", nativeToken: "ETH", blockExplorer: "https://testnet.arbiscan.io" },
+  421613: { name: "Arbitrum Goerli", nativeToken: "ETH", blockExplorer: "https://goerli.arbiscan.io" },
+  421614: { name: "Arbitrum Sepolia", nativeToken: "ETH", blockExplorer: "https://sepolia.arbiscan.io" },
+  11155111: { name: "Sepolia", nativeToken: "ETH", blockExplorer: "https://sepolia.ethercan.io" },
+  11155420: { name: "Optimism Sepolia", nativeToken: "ETH", blockExplorer: "https://sepolia-optimism.etherscan.io" },
+  168587773: { name: "Blast Sepolia", nativeToken: "ETH", blockExplorer: "https://testnet.blastscan.io" },
+};
