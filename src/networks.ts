23b5caf37a85b09fa9ed65a9466d5dd618e957dc
Paul <108695806+pxrl@users.noreply.github.com>
2024-03-08 11:35:53 +1100

    feat: Import public networks definitions from UMA (#20)
    Currently the Across FE needs to import PublicNetworks via
    @uma/common/dist due to an issue upstream. This change will instead
    provide chain definitions locally, where they be more easily managed.
    
    As part of this work, the chain definitions have been updated to include
    the latest Sepolia networks that are used by Across.
    
    See also:
    https://github.com/across-protocol/sdk-v2/commit/ca356ce0816922af33f14b99d5ca91e022ca0d59

--- src/networks.ts
+++ src/networks.ts
@@ -38,3 +38,38 @@ export const CHAIN_IDs = {
   ...MAINNET_CHAIN_IDs,
   ...TESTNET_CHAIN_IDs,
 };
+
+interface PublicNetwork {
+  name: string;
+  nativeToken: string;
+  blockExplorer: string;
+}
+
+export const PRODUCTION_NETWORKS: { [chainId: number]: PublicNetwork } = {
+  [CHAIN_IDs.ARBITRUM]: { name: "Arbitrum One", nativeToken: "ETH", blockExplorer: "https://arbiscan.io" },
+  [CHAIN_IDs.BASE]: { name: "Base", nativeToken: "ETH", blockExplorer: "https://basescan.org" },
+  [CHAIN_IDs.BOBA]: { name: "Boba", nativeToken: "ETH", blockExplorer: "https://blockexplorer.boba.network" },
+  [CHAIN_IDs.MAINNET]: { name: "Mainnet",  nativeToken: "ETH", blockExplorer: "https://etherscan.io" },
+  [CHAIN_IDs.OPTIMISM]: { name: "Optimism", nativeToken: "ETH", blockExplorer: "https://optimistic.ethercan.io" },
+  [CHAIN_IDs.POLYGON]: { name: "Polygon",  nativeToken: "MATIC", blockExplorer: "https://polygonscan.com" },
+  [CHAIN_IDs.ZK_SYNC]: { name: "zksync", nativeToken: "ETH", blockExplorer: "https://explorer.zksync.io" },
+};
+
+export const TEST_NETWORKS: { [chainId: number]: PublicNetwork } = {
+  [CHAIN_IDs.ARBITRUM_GOERLI]: { name: "Arbitrum Goerli", nativeToken: "ETH", blockExplorer: "https://goerli.arbiscan.io" },
+  [CHAIN_IDs.ARBITRUM_SEPOLIA]: { name: "Arbitrum Sepolia", nativeToken: "ETH", blockExplorer: "https://sepolia.arbiscan.io" },
+  [CHAIN_IDs.BASE_GOERLI]: { name: "Base Goerli", nativeToken: "ETH", blockExplorer: "https://goerli.basescan.org" },
+  [CHAIN_IDs.BASE_SEPOLIA]: { name: "Base Sepolia", nativeToken: "ETH", blockExplorer: "https://sepolia.basescan.org" },
+  [CHAIN_IDs.GOERLI]: { name: "Goerli", nativeToken: "ETH", blockExplorer: "https://goerli.ethercan.io" },
+  [CHAIN_IDs.MUMBAI]: { name: "Polygon Mumbai", nativeToken: "MATIC", blockExplorer: "https://mumbai.polygonscan.com" },
+  [CHAIN_IDs.OPTIMISM_SEPOLIA]: { name: "Optimism Sepolia", nativeToken: "ETH", blockExplorer: "https://sepolia-optimism.etherscan.io" },
+  [CHAIN_IDs.POLYGON_AMOY]: { name: "Polygon Amoy", nativeToken: "MATIC", blockExplorer: "https://oklink.com/amoy" },
+  [CHAIN_IDs.SEPOLIA]: { name: "Sepolia", nativeToken: "ETH", blockExplorer: "https://sepolia.ethercan.io" },
+  [CHAIN_IDs.ZK_SYNC_GOERLI]: { name: "zkSync Goerli", nativeToken: "ETH", blockExplorer: "https://goerli.explorer.zksync.io" },
+  [CHAIN_IDs.ZK_SYNC_SEPOLIA]: { name: "zkSync Sepolia", nativeToken: "ETH", blockExplorer: "https://sepolia.explorer.zksync.io" },
+};
+
+export const PUBLIC_NETWORKS = {
+  ...PRODUCTION_NETWORKS,
+  ...TEST_NETWORKS,
+};
