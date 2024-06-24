f79cbb1b8dc61aaacbf10374a3520bb2ecd82e2c
Paul <108695806+pxrl@users.noreply.github.com>
2024-06-24 11:55:24 +0200

    refactor: Drop Goerli definitions (#45)
    This is the first step in a small cleanup, prior to adding new chains.

--- src/networks.ts
+++ src/networks.ts
@@ -1,4 +1,4 @@
-// Chain names and ID's
+// Chain names and IDs.
 export const TESTNET_SEPOLIA_CHAIN_IDs = {
   ZK_SYNC_SEPOLIA: 300,
   MODE_SEPOLIA: 919,
@@ -10,19 +10,8 @@ export const TESTNET_SEPOLIA_CHAIN_IDs = {
   OPTIMISM_SEPOLIA: 11155420,
 };
 
-export const TESTNET_GOERLI_CHAIN_IDs = {
-  GOERLI: 5,
-  ZK_SYNC_GOERLI: 280,
-  OPTIMISM_GOERLI: 420,
-  MUMBAI: 80001,
-  BASE_GOERLI: 84531,
-  ARBITRUM_GOERLI: 421613,
-  LINEA_GOERLI: 59140
-};
-
 export const TESTNET_CHAIN_IDs = {
   ...TESTNET_SEPOLIA_CHAIN_IDs,
-  ...TESTNET_GOERLI_CHAIN_IDs,
 } as const;
 
 export const MAINNET_CHAIN_IDs = {
@@ -63,17 +52,12 @@ export const PRODUCTION_NETWORKS: { [chainId: number]: PublicNetwork } = {
 };
 
 export const TEST_NETWORKS: { [chainId: number]: PublicNetwork } = {
-  [CHAIN_IDs.ARBITRUM_GOERLI]: { name: "Arbitrum Goerli", nativeToken: "ETH", blockExplorer: "https://goerli.arbiscan.io" },
   [CHAIN_IDs.ARBITRUM_SEPOLIA]: { name: "Arbitrum Sepolia", nativeToken: "ETH", blockExplorer: "https://sepolia.arbiscan.io" },
-  [CHAIN_IDs.BASE_GOERLI]: { name: "Base Goerli", nativeToken: "ETH", blockExplorer: "https://goerli.basescan.org" },
   [CHAIN_IDs.BASE_SEPOLIA]: { name: "Base Sepolia", nativeToken: "ETH", blockExplorer: "https://sepolia.basescan.org" },
-  [CHAIN_IDs.GOERLI]: { name: "Goerli", nativeToken: "ETH", blockExplorer: "https://goerli.etherscan.io" },
-  [CHAIN_IDs.MUMBAI]: { name: "Polygon Mumbai", nativeToken: "MATIC", blockExplorer: "https://mumbai.polygonscan.com" },
   [CHAIN_IDs.OPTIMISM_SEPOLIA]: { name: "Optimism Sepolia", nativeToken: "ETH", blockExplorer: "https://sepolia-optimism.etherscan.io" },
   [CHAIN_IDs.POLYGON_AMOY]: { name: "Polygon Amoy", nativeToken: "MATIC", blockExplorer: "https://amoy.polygonscan.com/" },
   [CHAIN_IDs.SCROLL_SEPOLIA]: { name: "Scroll Sepolia", nativeToken: "ETH", blockExplorer: "https://sepolia.scrollscan.com/" },
   [CHAIN_IDs.SEPOLIA]: { name: "Sepolia", nativeToken: "ETH", blockExplorer: "https://sepolia.etherscan.io" },
-  [CHAIN_IDs.ZK_SYNC_GOERLI]: { name: "zkSync Goerli", nativeToken: "ETH", blockExplorer: "https://goerli.explorer.zksync.io" },
   [CHAIN_IDs.ZK_SYNC_SEPOLIA]: { name: "zkSync Sepolia", nativeToken: "ETH", blockExplorer: "https://sepolia-era.zksync.network/" },
   [CHAIN_IDs.MODE_SEPOLIA]: { name: "Mode Sepolia", nativeToken: "ETH", blockExplorer: "https://sepolia.explorer.mode.network" },
 };
