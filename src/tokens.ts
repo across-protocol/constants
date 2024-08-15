321637b766dfe8e1b5bf80b29039c1b8a393869e
Paul <108695806+pxrl@users.noreply.github.com>
2024-08-15 18:13:07 +0200

    fix(Zora): Map USDzC to USDC (#69)

--- src/tokens.ts
+++ src/tokens.ts
@@ -331,5 +331,6 @@ export const TOKEN_SYMBOLS_MAP = {
 export const TOKEN_EQUIVALENCE_REMAPPING: { [symbol: string]: string } = {
   [TOKEN_SYMBOLS_MAP["USDC.e"].symbol]: TOKEN_SYMBOLS_MAP["USDC"].symbol,
   [TOKEN_SYMBOLS_MAP["USDbC"].symbol]: TOKEN_SYMBOLS_MAP["USDC"].symbol,
+  [TOKEN_SYMBOLS_MAP["USDzC"].symbol]: TOKEN_SYMBOLS_MAP["USDC"].symbol,
   [TOKEN_SYMBOLS_MAP["USDB"].symbol]: TOKEN_SYMBOLS_MAP["DAI"].symbol,
 };
