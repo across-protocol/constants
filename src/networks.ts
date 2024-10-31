14812b9d436d36fa8582a6ad4caaec18173050ce
Dong-Ha Kim <dongha.kim210@gmail.com>
2024-10-31 17:06:28 +0100

    feat: use viem chains

--- src/networks.ts
+++ src/networks.ts
@@ -1,41 +1,133 @@
+import { Chain, defineChain } from "viem";
+import {
+  arbitrum,
+  arbitrumSepolia,
+  base,
+  baseSepolia,
+  blast,
+  blastSepolia,
+  boba,
+  linea,
+  lisk,
+  liskSepolia,
+  mainnet,
+  mode,
+  modeTestnet,
+  optimism,
+  optimismSepolia,
+  polygon,
+  polygonAmoy,
+  redstone,
+  scroll,
+  scrollSepolia,
+  sepolia,
+  worldchain,
+  zksync,
+  zksyncSepoliaTestnet,
+  zora,
+} from "viem/chains";
+
+type TestnetChainKey = keyof typeof TESTNET_SEPOLIA_CHAINS;
+type MainnetChainKey = keyof typeof MAINNET_CHAINS;
+
+// Custom chains
+const alephZero = defineChain({
+  id: 41_455,
+  name: "Aleph Zero",
+  nativeCurrency: {
+    name: "Aleph Zero",
+    symbol: "AZERO",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc.alephzero.org"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "EVM Explorer",
+      url: "https://evm-explorer.alephzero.org",
+      apiUrl: "https://evm-explorer.alephzero.org/api",
+    },
+  },
+});
+const superseed = defineChain({
+  id: 5330,
+  name: "Superseed",
+  nativeCurrency: {
+    name: "Ether",
+    symbol: "ETH",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: [""], // TODO
+    },
+  },
+  blockExplorers: {
+    default: {
+      // TODO
+      name: "",
+      url: "",
+      apiUrl: "",
+    },
+  },
+});
+
 // Chain names and IDs.
-export const TESTNET_SEPOLIA_CHAIN_IDs = {
-  ARBITRUM_SEPOLIA: 421614,
-  BASE_SEPOLIA: 84532,
-  BLAST_SEPOLIA: 168587773,
-  LISK_SEPOLIA: 4202,
-  MODE_SEPOLIA: 919,
-  OPTIMISM_SEPOLIA: 11155420,
-  POLYGON_AMOY: 80002,
-  SCROLL_SEPOLIA: 534351,
-  SEPOLIA: 11155111,
-  ZK_SYNC_SEPOLIA: 300,
+export const TESTNET_SEPOLIA_CHAINS = {
+  ARBITRUM_SEPOLIA: arbitrumSepolia,
+  BASE_SEPOLIA: baseSepolia,
+  BLAST_SEPOLIA: blastSepolia,
+  LISK_SEPOLIA: liskSepolia,
+  MODE_SEPOLIA: modeTestnet,
+  OPTIMISM_SEPOLIA: optimismSepolia,
+  POLYGON_AMOY: polygonAmoy,
+  SCROLL_SEPOLIA: scrollSepolia,
+  SEPOLIA: sepolia,
+  ZK_SYNC_SEPOLIA: zksyncSepoliaTestnet,
 };
+
+export const MAINNET_CHAINS = {
+  ALEPH_ZERO: alephZero,
+  ARBITRUM: arbitrum,
+  BASE: base,
+  BLAST: blast,
+  BOBA: boba,
+  LINEA: linea,
+  LISK: lisk,
+  MAINNET: mainnet,
+  MODE: mode,
+  OPTIMISM: optimism,
+  POLYGON: polygon,
+  REDSTONE: redstone,
+  SCROLL: scroll,
+  SUPERSEED: superseed,
+  WORLD_CHAIN: worldchain,
+  ZK_SYNC: zksync,
+  ZORA: zora,
+};
+
+export const TESTNET_SEPOLIA_CHAIN_IDs = Object.entries(TESTNET_SEPOLIA_CHAINS).reduce(
+  (acc, [key, chain]) => {
+    acc[key as TestnetChainKey] = chain.id;
+    return acc;
+  },
+  {} as Record<TestnetChainKey, number>,
+);
 
 export const TESTNET_CHAIN_IDs = {
   ...TESTNET_SEPOLIA_CHAIN_IDs,
 } as const;
 
-export const MAINNET_CHAIN_IDs = {
-  ALEPH_ZERO: 41455,
-  ARBITRUM: 42161,
-  BASE: 8453,
-  BLAST: 81457,
-  BOBA: 288,
-  LINEA: 59144,
-  LISK: 1135,
-  MAINNET: 1,
-  MODE: 34443,
-  OPTIMISM: 10,
-  POLYGON: 137,
-  REDSTONE: 690,
-  SCROLL: 534352,
-  SUPERSEED: 5330,
-  WORLD_CHAIN: 480,
-  ZK_SYNC: 324,
-  ZORA: 7777777
-};
-
+export const MAINNET_CHAIN_IDs = Object.entries(MAINNET_CHAINS).reduce(
+  (acc, [key, chain]) => {
+    acc[key as MainnetChainKey] = chain.id;
+    return acc;
+  },
+  {} as Record<MainnetChainKey, number>,
+);
 export const CHAIN_IDs = {
   ...MAINNET_CHAIN_IDs,
   ...TESTNET_CHAIN_IDs,
@@ -44,7 +136,26 @@ export const CHAIN_IDs = {
 export enum ChainFamily {
   OP_STACK,
   ORBIT, // Future: Might need to distinguish between ORBIT_L2 and ORBIT_L3...
-};
+}
+
+export const OP_STACK_CHAIN_IDs = [
+  CHAIN_IDs.BASE,
+  CHAIN_IDs.BASE_SEPOLIA,
+  CHAIN_IDs.BLAST,
+  CHAIN_IDs.BLAST_SEPOLIA,
+  CHAIN_IDs.LISK,
+  CHAIN_IDs.LISK_SEPOLIA,
+  CHAIN_IDs.MODE,
+  CHAIN_IDs.MODE_SEPOLIA,
+  CHAIN_IDs.OPTIMISM,
+  CHAIN_IDs.OPTIMISM_SEPOLIA,
+  CHAIN_IDs.REDSTONE,
+  CHAIN_IDs.SUPERSEED,
+  CHAIN_IDs.WORLD_CHAIN,
+  CHAIN_IDs.ZORA,
+];
+
+export const ORBIT_CHAIN_IDs = [CHAIN_IDs.ALEPH_ZERO];
 
 interface PublicNetwork {
   name: string;
@@ -54,163 +165,39 @@ interface PublicNetwork {
 }
 
 const { ORBIT, OP_STACK } = ChainFamily;
-export const PRODUCTION_NETWORKS: { [chainId: number]: PublicNetwork } = {
-  [CHAIN_IDs.ALEPH_ZERO]: {
-    name: "Aleph Zero",
-    family: ORBIT,
-    nativeToken: "AZERO",
-    blockExplorer: "https://evm-explorer.alephzero.org",
-  },
-  [CHAIN_IDs.ARBITRUM]: {
-    name: "Arbitrum One",
-    nativeToken: "ETH",
-    blockExplorer: "https://arbiscan.io"
-  },
-  [CHAIN_IDs.BASE]: {
-    name: "Base",
-    family: OP_STACK,
-    nativeToken: "ETH",
-    blockExplorer: "https://basescan.org"
-  },
-  [CHAIN_IDs.BLAST]: {
-    name: "Blast",
-    family: OP_STACK,
-    nativeToken: "ETH",
-    blockExplorer: "https://blastscan.io"
-  },
-  [CHAIN_IDs.BOBA]: {
-    name: "Boba",
-    nativeToken: "ETH",
-    blockExplorer: "https://blockexplorer.boba.network"
-  },
-  [CHAIN_IDs.LINEA]: {
-    name: "Linea",
-    nativeToken: "ETH",
-    blockExplorer: "https://lineascan.build"
-  },
-  [CHAIN_IDs.LISK]: {
-    name: "Lisk",
-    family: OP_STACK,
-    nativeToken: "ETH",
-    blockExplorer: "https://blockscout.lisk.com"
-  },
-  [CHAIN_IDs.MAINNET]: {
-    name: "Mainnet",
-    nativeToken: "ETH",
-    blockExplorer: "https://etherscan.io"
-  },
-  [CHAIN_IDs.MODE]: {
-    name: "Mode",
-    family: OP_STACK,
-    nativeToken: "ETH",
-    blockExplorer: "https://explorer.mode.network"
-  },
-  [CHAIN_IDs.OPTIMISM]: {
-    name: "Optimism",
-    family: OP_STACK,
-    nativeToken: "ETH",
-    blockExplorer: "https://optimistic.etherscan.io"
-  },
-  [CHAIN_IDs.POLYGON]: {
-    name: "Polygon",
-    nativeToken: "MATIC",
-    blockExplorer: "https://polygonscan.com"
-  },
-  [CHAIN_IDs.REDSTONE]: {
-    name: "Redstone",
-    nativeToken: "ETH",
-    blockExplorer: "https://explorer.redstone.xyz",
-    family: OP_STACK,
-  },
-  [CHAIN_IDs.SCROLL]: {
-    name: "Scroll",
-    nativeToken: "ETH",
-    blockExplorer: "https://scrollscan.com"
-  },
-  [CHAIN_IDs.SUPERSEED]: {
-    name: "Superseed",
-    nativeToken: "ETH",
-    blockExplorer: "", // @todo: To be added later
-    family: OP_STACK
-  },
-  [CHAIN_IDs.WORLD_CHAIN]: {
-    name: "World Chain",
-    nativeToken: "ETH",
-    blockExplorer: "https://worldchain-mainnet-explorer.alchemy.com",
-    family: OP_STACK
-  },
-  [CHAIN_IDs.ZK_SYNC]: {
-    name: "zkSync",
-    nativeToken: "ETH",
-    blockExplorer: "https://era.zksync.network"
-  },
-  [CHAIN_IDs.ZORA]: {
-    name: "Zora",
-    nativeToken: "ETH",
-    blockExplorer: "https://zorascan.xyz",
-    family: OP_STACK
-  }
-};
 
-export const TEST_NETWORKS: { [chainId: number]: PublicNetwork } = {
-  [CHAIN_IDs.ARBITRUM_SEPOLIA]: {
-    name: "Arbitrum Sepolia",
-    nativeToken: "ETH",
-    blockExplorer: "https://sepolia.arbiscan.io"
+export const PRODUCTION_NETWORKS = Object.values(MAINNET_CHAINS).reduce(
+  (acc, chain) => {
+    const publicNetwork = definePublicNetwork(chain);
+    acc[chain.id] = publicNetwork;
+    return acc;
   },
-  [CHAIN_IDs.BASE_SEPOLIA]: {
-    name: "Base Sepolia",
-    family: OP_STACK,
-    nativeToken: "ETH",
-    blockExplorer: "https://sepolia.basescan.org"
-  },
-  [CHAIN_IDs.BLAST_SEPOLIA]: {
-    name: "Blast Sepolia",
-    family: OP_STACK,
-    nativeToken: "ETH",
-    blockExplorer: "https://sepolia.blastscan.io"
-  },
-  [CHAIN_IDs.LISK_SEPOLIA]: {
-    name: "Lisk Sepolia",
-    family: OP_STACK,
-    nativeToken: "ETH",
-    blockExplorer: "https://sepolia-blockscout.lisk.com"
-  },
-  [CHAIN_IDs.MODE_SEPOLIA]: {
-    name: "Mode Sepolia",
-    family: OP_STACK,
-    nativeToken: "ETH",
-    blockExplorer: "https://sepolia.explorer.mode.network"
-  },
-  [CHAIN_IDs.OPTIMISM_SEPOLIA]: {
-    name: "Optimism Sepolia",
-    family: OP_STACK,
-    nativeToken: "ETH",
-    blockExplorer: "https://sepolia-optimism.etherscan.io"
-  },
-  [CHAIN_IDs.POLYGON_AMOY]: {
-    name: "Polygon Amoy",
-    nativeToken: "MATIC",
-    blockExplorer: "https://amoy.polygonscan.com"
-  },
-  [CHAIN_IDs.SCROLL_SEPOLIA]: {
-    name: "Scroll Sepolia",
-    nativeToken: "ETH",
-    blockExplorer: "https://sepolia.scrollscan.com"
-  },
-  [CHAIN_IDs.SEPOLIA]: {
-    name: "Sepolia",
-    nativeToken: "ETH",
-    blockExplorer: "https://sepolia.etherscan.io"
-  },
-  [CHAIN_IDs.ZK_SYNC_SEPOLIA]: {
-    name: "zkSync Sepolia",
-    nativeToken: "ETH",
-    blockExplorer: "https://sepolia-era.zksync.network"
-  }
-};
+  {} as Record<number, PublicNetwork>,
+);
 
+export const TEST_NETWORKS = Object.values(TESTNET_SEPOLIA_CHAINS).reduce(
+  (acc, chain) => {
+    const publicNetwork = definePublicNetwork(chain);
+    acc[chain.id] = publicNetwork;
+    return acc;
+  },
+  {} as Record<number, PublicNetwork>,
+);
 export const PUBLIC_NETWORKS = {
   ...PRODUCTION_NETWORKS,
   ...TEST_NETWORKS,
 };
+
+function definePublicNetwork(chain: Chain): PublicNetwork {
+  const family = OP_STACK_CHAIN_IDs.includes(chain.id)
+    ? OP_STACK
+    : ORBIT_CHAIN_IDs.includes(chain.id)
+    ? ORBIT
+    : undefined;
+  return {
+    name: chain.name,
+    nativeToken: chain.nativeCurrency.symbol,
+    blockExplorer: chain.blockExplorers?.default.url ?? "",
+    family,
+  };
+}
