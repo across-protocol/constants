4dbae517dcb1b530f9756b80dae290f69f0f8e7f
Paul <108695806+pxrl@users.noreply.github.com>
2025-01-02 13:07:13 +0000

    feat: Require public RPC defintions
    To be used by the contracts repository, and certain scripts in the
    relayer repository.

--- src/networks.ts
+++ src/networks.ts
@@ -53,6 +53,7 @@ interface PublicNetwork {
   name: string;
   family: ChainFamily;
   nativeToken: string;
+  publicRPC: string; // RPC provider of last resort.
   blockExplorer: string;
 }
 
@@ -62,108 +63,126 @@ export const PRODUCTION_NETWORKS: { [chainId: number]: PublicNetwork } = {
     name: "Aleph Zero",
     family: ORBIT,
     nativeToken: "AZERO",
+    publicRPC: "https://rpc.alephzero.raas.gelato.cloud",
     blockExplorer: "https://evm-explorer.alephzero.org",
   },
   [CHAIN_IDs.ARBITRUM]: {
     name: "Arbitrum One",
     family: NONE,
     nativeToken: "ETH",
+    publicRPC: "https://arb1.arbitrum.io/rpc",
     blockExplorer: "https://arbiscan.io"
   },
   [CHAIN_IDs.BASE]: {
     name: "Base",
     family: OP_STACK,
     nativeToken: "ETH",
+    publicRPC: "https://mainnet.base.org",
     blockExplorer: "https://basescan.org"
   },
   [CHAIN_IDs.BLAST]: {
     name: "Blast",
     family: OP_STACK,
     nativeToken: "ETH",
+    publicRPC: "https://rpc.blast.io",
     blockExplorer: "https://blastscan.io"
   },
   [CHAIN_IDs.BOBA]: {
     name: "Boba",
     family: OP_STACK,
     nativeToken: "ETH",
+    publicRPC: "https://mainnet.boba.network",
     blockExplorer: "https://blockexplorer.boba.network"
   },
   [CHAIN_IDs.INK]: {
     name: "Ink",
     family: OP_STACK,
     nativeToken: "ETH",
+    publicRPC: "https://rpc-gel.inkonchain.com",
     blockExplorer: "https://explorer.inkonchain.com"
   },
   [CHAIN_IDs.LINEA]: {
     name: "Linea",
     family: NONE,
     nativeToken: "ETH",
+    publicRPC: "https://rpc.linea.build",
     blockExplorer: "https://lineascan.build"
   },
   [CHAIN_IDs.LISK]: {
     name: "Lisk",
     family: OP_STACK,
     nativeToken: "ETH",
+    publicRPC: "https://rpc.api.lisk.com",
     blockExplorer: "https://blockscout.lisk.com"
   },
   [CHAIN_IDs.MAINNET]: {
     name: "Mainnet",
     family: NONE,
     nativeToken: "ETH",
+    publicRPC: "https://eth.llamarpc.com",
     blockExplorer: "https://etherscan.io"
   },
   [CHAIN_IDs.MODE]: {
     name: "Mode",
     family: OP_STACK,
     nativeToken: "ETH",
+    publicRPC: "https://mainnet.mode.network",
     blockExplorer: "https://explorer.mode.network"
   },
   [CHAIN_IDs.OPTIMISM]: {
     name: "Optimism",
     family: OP_STACK,
     nativeToken: "ETH",
+    publicRPC: "https://mainnet.optimism.io",
     blockExplorer: "https://optimistic.etherscan.io"
   },
   [CHAIN_IDs.POLYGON]: {
     name: "Polygon",
     family: NONE,
     nativeToken: "MATIC",
+    publicRPC: "https://polygon-rpc.com",
     blockExplorer: "https://polygonscan.com"
   },
   [CHAIN_IDs.REDSTONE]: {
     name: "Redstone",
     family: OP_STACK,
     nativeToken: "ETH",
+    publicRPC: "https://rpc.redstonechain.com ",
     blockExplorer: "https://explorer.redstone.xyz",
   },
   [CHAIN_IDs.SCROLL]: {
     name: "Scroll",
     family: NONE,
     nativeToken: "ETH",
+    publicRPC: "https://rpc.scroll.io",
     blockExplorer: "https://scrollscan.com"
   },
   [CHAIN_IDs.SUPERSEED]: {
     name: "Superseed",
     family: OP_STACK,
     nativeToken: "ETH",
+    publicRPC: "https://mainnet.superseed.xyz",
     blockExplorer: "" // @todo: To be added later
   },
   [CHAIN_IDs.WORLD_CHAIN]: {
     name: "World Chain",
     family: OP_STACK,
     nativeToken: "ETH",
+    publicRPC: "https://worldchain-mainnet.g.alchemy.com/public",
     blockExplorer: "https://worldchain-mainnet-explorer.alchemy.com",
   },
   [CHAIN_IDs.ZK_SYNC]: {
     name: "zkSync",
     family: NONE,
     nativeToken: "ETH",
+    publicRPC: "https://mainnet.era.zksync.io",
     blockExplorer: "https://era.zksync.network"
   },
   [CHAIN_IDs.ZORA]: {
     name: "Zora",
     family: OP_STACK,
     nativeToken: "ETH",
+    publicRPC: "https://rpc.zora.energy",
     blockExplorer: "https://zorascan.xyz",
   }
 };
@@ -173,60 +192,70 @@ export const TEST_NETWORKS: { [chainId: number]: PublicNetwork } = {
     name: "Arbitrum Sepolia",
     family: NONE,
     nativeToken: "ETH",
+    publicRPC: "https://sepolia-rollup.arbitrum.io/rpc",
     blockExplorer: "https://sepolia.arbiscan.io"
   },
   [CHAIN_IDs.BASE_SEPOLIA]: {
     name: "Base Sepolia",
     family: OP_STACK,
     nativeToken: "ETH",
+    publicRPC: "https://sepolia.base.org",
     blockExplorer: "https://sepolia.basescan.org"
   },
   [CHAIN_IDs.BLAST_SEPOLIA]: {
     name: "Blast Sepolia",
     family: OP_STACK,
     nativeToken: "ETH",
+    publicRPC: "https://sepolia.blast.io",
     blockExplorer: "https://sepolia.blastscan.io"
   },
   [CHAIN_IDs.LISK_SEPOLIA]: {
     name: "Lisk Sepolia",
     family: OP_STACK,
     nativeToken: "ETH",
+    publicRPC: "https://rpc.sepolia-api.lisk.com",
     blockExplorer: "https://sepolia-blockscout.lisk.com"
   },
   [CHAIN_IDs.MODE_SEPOLIA]: {
     name: "Mode Sepolia",
     family: OP_STACK,
     nativeToken: "ETH",
+    publicRPC: "https://sepolia.mode.network",
     blockExplorer: "https://sepolia.explorer.mode.network"
   },
   [CHAIN_IDs.OPTIMISM_SEPOLIA]: {
     name: "Optimism Sepolia",
     family: OP_STACK,
     nativeToken: "ETH",
+    "rpc": "https://sepolia.optimism.io",
     blockExplorer: "https://sepolia-optimism.etherscan.io"
   },
   [CHAIN_IDs.POLYGON_AMOY]: {
     name: "Polygon Amoy",
     family: NONE,
     nativeToken: "MATIC",
+    publicRPC: "https://rpc-amoy.polygon.technology",
     blockExplorer: "https://amoy.polygonscan.com"
   },
   [CHAIN_IDs.SCROLL_SEPOLIA]: {
     name: "Scroll Sepolia",
     family: NONE,
     nativeToken: "ETH",
+    publicRPC: "https://sepolia-rpc.scroll.io",
     blockExplorer: "https://sepolia.scrollscan.com"
   },
   [CHAIN_IDs.SEPOLIA]: {
     name: "Sepolia",
     family: NONE,
     nativeToken: "ETH",
+    publicRPC: "https://sepolia.drpc.org",
     blockExplorer: "https://sepolia.etherscan.io"
   },
   [CHAIN_IDs.ZK_SYNC_SEPOLIA]: {
     name: "zkSync Sepolia",
     family: NONE,
     nativeToken: "ETH",
+    publicRPC: "https://sepolia.era.zksync.dev",
     blockExplorer: "https://sepolia-era.zksync.network"
   }
 };
