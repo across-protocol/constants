bb729afab0d7003dbb09141ecad2e863ed82a689
Gerhard Steenkamp <gerhard@umaproject.org>
2025-01-09 14:24:30 +0200

    fix lint and format script

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,16 @@ module.exports = {
     browser: false,
     es2021: true,
     mocha: true,
-    node: true,
+    node: true
   },
   plugins: ["@typescript-eslint"],
   extends: ["standard", "plugin:prettier/recommended", "plugin:node/recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 12
   },
   rules: {
     "node/no-unsupported-features/es-syntax": ["error", { ignores: ["modules"] }],
-    "@typescript-eslint/no-var-requires": 0,
-  },
+    "@typescript-eslint/no-var-requires": 0
+  }
 };
